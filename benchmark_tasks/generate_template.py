d1f8c5379b9527de275a1aab35e4440d55300dbe
Grant <tgrant@syr.gov>
2024-02-14 15:34:25 -0500

    Fixed templates

--- benchmark_tasks/generate_template.py
+++ benchmark_tasks/generate_template.py
@@ -11,18 +11,25 @@ def main(id_prefixes=[10, 20]):
     
     subj_ids = generate_subj_ids(id_prefixes)
 
+    _subj_ids = []
+    for elm in subj_ids:
+        _subj_ids.append(elm)
+        _subj_ids.append(elm)
+
+    print(_subj_ids)
+
     sessions = ["1", "2"]
     templates = ["1", "2", "3", "4"]
-    practice = ["0" for elm in subj_ids]
-    ses = [sessions[i % len(sessions)] for i, elm in enumerate(subj_ids)]
-    temp = [templates[i % len(templates)] for i, elm in enumerate(subj_ids)]
+    practice = ["0" for elm in _subj_ids]
+    ses = [sessions[i % len(sessions)] for i, elm in enumerate(_subj_ids)]
+    temp = [templates[i % len(templates)] for i, elm in enumerate(_subj_ids)]
 
     with open ("out.csv", "w", newline='') as out_fp:
         writer = csv.writer(out_fp, delimiter=",")
 
         writer.writerow(["subject_id", "session_num", "template", "test"])
 
-        for i, elm in enumerate(subj_ids):
+        for i, elm in enumerate(_subj_ids):
             writer.writerow([elm, ses[i], temp[i], "0"])
         
     
